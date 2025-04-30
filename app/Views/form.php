84b8941839f80fa9e7b073220838981943638fab
MiguelSommerf <miguel.sommerfeldluiz@gmail.com>
2025-04-30 14:28:56 -0300

    Modificando rotas e adicionando views 'form' e 'home'. Ainda não foi testado, pois o PHP da máquina utilizada está desatualizado.

---
+++ app/Views/form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <form action="<?= base_url('validarLogin') ?>" method="post">
+        <label>E-mail</label>
+        <input type="text" name="email">
+        <label>Senha</label>
+        <input type="password" name="">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
